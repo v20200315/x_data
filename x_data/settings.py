dc85bdc11590929d666664cad796cf9882dc1155
v20200315 <v20200315@gmail.com>
2024-10-23 11:01:33 +0800

    init stocks model

--- x_data/settings.py
+++ x_data/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "stocks.apps.StocksConfig",
     "samples.apps.SamplesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
